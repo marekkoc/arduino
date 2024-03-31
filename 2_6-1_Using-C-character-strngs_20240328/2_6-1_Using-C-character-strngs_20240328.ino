5f941436dfa0538b0d4b8983545806c079af6468
marekkoc <m2019k1020@gmail.com>
2024-03-31 11:22:56 +0200

    2.6.1 Using C-character strings

---
+++ 2_6-1_Using-C-character-strngs_20240328/2_6-1_Using-C-character-strngs_20240328.ino
@@ -0,0 +1,27 @@
+/*
+Book: 2020#Margolis_Arduino-Cookbook.pdf
+Chapter 2.6: page:55
+
+Sketch: Using C Character Strings
+
+* Basic_Strings sketch
+ 
+* My modification:
+* oryginal book version
+
+
+MK:
+C:2024.03.28
+M:2020.03.28
+*/
+
+
+oid setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
