c53dc52cdc1d1304e04a9eedcebb209fdff78076
marekkoc <m2019k1020@gmail.com>
2024-03-31 11:20:41 +0200

    2.4 Group of values in an arrays- On/Off 2 LEDs

---
+++ 2_4_groups_of_values_arrays_2-diody_20240315/2_4_groups_of_values_arrays_2-diody_20240315.ino
@@ -0,0 +1,47 @@
+/*
+Book: 2020#Margolis_Arduino-Cookbook.pdf
+Chapter 2.4: page:45
+
+Sketch: Working wit Groups of Values
+
+* array sketch
+* an array of switches controls an array of LEDs
+* see Chapter 5 for more on using switches
+* see Chapter 7 for information on LEDs
+
+* My modification
+* Turn on two LEDs in parallel: this from the button and the neighbouring one.
+
+MK:
+C:2024.03.15
+M:2020.03.15
+*/
+
+ int inputPins[] = {2, 3, 4, 5}; // create an array of pins for switch inputs
+ int ledPins[] = {10, 11, 12, 13}; // create an array of output pins for leds
+
+
+ void setup()
+ {
+  for (int index = 0; index < 4; index++)
+  {    
+    pinMode(inputPins[index], INPUT_PULLUP); // declare as input
+    pinMode(ledPins[index], OUTPUT); // declare LED as output
+  }
+ }
+
+ void loop(){
+  for (int index = 0; index < 4; index++)
+  {
+    int idx_next = (index + 1) % 4;
+    int val = digitalRead(inputPins[index]); // read input value
+    if (val == LOW)                          // check if the switch is pressed
+    {
+      digitalWrite(ledPins[index], HIGH);   // LED on if the switch is pressed
+      digitalWrite(ledPins[idx_next], HIGH);   // LED on if the switch is pressed
+    }
+    else {
+    digitalWrite(ledPins[index], LOW);    // turn LED off
+    }
+  }
+ }
