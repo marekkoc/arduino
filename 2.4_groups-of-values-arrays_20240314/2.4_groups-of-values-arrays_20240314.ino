8a944c8d3b92927864aae312de0ceb78ee1deb50
marekkoc <m2019k1020@gmail.com>
2024-03-31 11:19:39 +0200

    2.4 Group of values in an arrays

---
+++ 2.4_groups-of-values-arrays_20240314/2.4_groups-of-values-arrays_20240314.ino
@@ -0,0 +1,42 @@
+/*
+Book: 2020#Margolis_Arduino-Cookbook.pdf
+Chapter 2.4: page:45
+
+Sketch: Working wit Groups of Values
+
+* array sketch
+* an array of switches controls an array of LEDs
+* see Chapter 5 for more on using switches
+* see Chapter 7 for information on LEDs
+
+MK:
+C:2024.03.14
+M:2020.03.14
+*/
+
+ int inputPins[] = {2, 3, 4, 5}; // create an array of pins for switch inputs
+ int ledPins[] = {10, 11, 12, 13}; // create an array of output pins for leds
+
+
+ void setup()
+ {
+  for (int index = 0; index < 4; index++)
+  {    
+    pinMode(inputPins[index], INPUT_PULLUP); // declare as input
+    pinMode(ledPins[index], OUTPUT); // declare LED as output
+  }
+ }
+
+ void loop(){
+  for (int index = 0; index < 4; index++)
+  {
+    int val = digitalRead(inputPins[index]); // read input value
+    if (val == LOW)                          // check if the switch is pressed
+    {
+      digitalWrite(ledPins[index], HIGH);   // LED on if the switch is pressed
+    }
+    else {
+    digitalWrite(ledPins[index], LOW);    // turn LED off
+    }
+  }
+ }
